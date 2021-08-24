13f8296b4403d478427445643c11329a8d5400a4
Pleyades-stack <84481380+Pleyades-stack@users.noreply.github.com>
2021-08-24 19:03:16 -0400

    modelo de usuario

--- src/models.py
+++ src/models.py
@@ -2,18 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Usuario(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    tipo= db.Column(db.String(10), unique=False, nullable=False)
+    nombre= db.Column(db.String(15), unique=False, nullable=False)
+    apellido= db.Column(db.String(15), unique=False, nullable=False)
+    sexo= db.Column(db.String(10), unique=False, nullable=False)
+    correo= db.Column(db.String(50), unique=True, nullable=False)
+    clave= db.Column(db.String(8), unique=False, nullable=False)
+    telefono= db.Column(db.String(11), unique=False, nullable=False)
+    ciudad= db.Column(db.String(20), unique=False, nullable=False)
+    rrss= db.Column(db.String(50), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<usuario %r>' % self.nombreusuario
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "tipo": self.tipo,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "sexo": self.sexo,
+            "correo": self.correo,
+            "telefono": self.telefono,
+            "ciudad": self.ciudad,
+            "rrss": self.rrss,
             # do not serialize the password, its a security breach
-        }
+        }      
