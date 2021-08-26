50e3e2642e46056440310d193aa40a8a0f421c33
Pleyades-stack <84481380+Pleyades-stack@users.noreply.github.com>
2021-08-26 19:10:07 -0400

    cambios en el modelo usuario

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Usuario
 #from models import Person
 
 app = Flask(__name__)
@@ -30,7 +30,7 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/usuario', methods=['GET'])
 def handle_hello():
 
     response_body = {
